13e2708d74376f45b2bf8c7ac64ccc0ae8802309
Marty Thierry <marty@martylouis.com>
2020-10-05 18:11:11 -0500

    update
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,3 @@
-import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
-import 'swiper/components/scrollbar/scrollbar.scss';
-import 'swiper/components/effect-coverflow/effect-coverflow.scss';
 import '../styles/main.css';
 
 export default function App({ Component, pageProps }) {
