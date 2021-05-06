46fbf1702d93cfa1b14a9d446d185b035879d9f6
Marty Thierry <marty@martylouis.com>
2021-05-06 13:02:59 -0500

    build(webpack): update config
    Unset client-side js that only works server-side

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, module: false };
+    return config;
+  },
   images: {
     domains: ['static.martylouis.com', 'images.unsplash.com'],
   },
