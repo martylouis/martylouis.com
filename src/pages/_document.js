33c0c11611a14388c9a9f6bec3a9d800ce59414b
Marty Thierry <marty@martylouis.com>
2021-05-20 14:37:39 -0500

    feat: setup stitches with tw macro

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,27 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { getCssString } from 'stitches.config';
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    try {
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            <style
+              id="stitches"
+              dangerouslySetInnerHTML={{ __html: getCssString() }}
+            />
+          </>
+        ),
+      };
+    } finally {
+    }
+  }
+
   render() {
     return (
       <>
