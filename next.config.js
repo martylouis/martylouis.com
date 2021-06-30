6fbbd0d30df1ce9e22abd4edd3e5aeb037b48402
Marty Thierry <marty@martylouis.com>
2021-06-30 16:00:50 -0500

    chore: udpate next.config.js
    `future.webpack5` in `next.config.js` has moved to the top level `webpack5` flag https://nextjs.org/docs/messages/future-webpack5-moved-to-webpack5
    
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, module: false };
     return config;
