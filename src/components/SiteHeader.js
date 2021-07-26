67667b72703f63b43de443696f2ae29583e93652
Marty Thierry <marty@martylouis.com>
2021-07-26 00:00:21 -0500

    wip: update header
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -7,23 +7,26 @@ import { header as headerNav } from '@data/nav';
 
 const SiteHeader = () => {
   return (
-    <header tw="py-4 backdrop-filter backdrop-blur w-full fixed z-50">
-      <Container size="lg" tw="flex items-center justify-between">
-        <Link href="/" passHref>
-          <a tw="inline-block hocus:(text-green-500 outline-none)">
-            <span tw="block w-10 h-10">
-              <Logo title="Marty Louis logo" />
-            </span>
-            {/* <span className="ml-2 span">martylouis.com</span> */}
-          </a>
-        </Link>
-        <div tw="flex items-center">
-          <div tw="flex mr-6">
-            {headerNav?.map(({ title, url }) => (
-              <Link passHref href={url} key={title}>
-                <StyledLink>{title}</StyledLink>
-              </Link>
-            ))}
+    <header tw="fixed z-50 w-full py-3 backdrop-filter backdrop-blur-lg">
+      <Container isGrid>
+        <div tw="absolute inset-0 transition-all w-full h-full bg-gray-100 bg-opacity-80 dark:(bg-gray-800 bg-opacity-50)"></div>
+        <div tw="flex items-center justify-between relative">
+          <div tw="flex items-center">
+            <Link href="/" passHref>
+              <a tw="inline-block hocus:(text-green-500 outline-none)">
+                <span tw="block w-10 h-10">
+                  <Logo title="Marty Louis logo" />
+                </span>
+                {/* <span className="ml-2 span">martylouis.com</span> */}
+              </a>
+            </Link>
+            <div tw="flex ml-4">
+              {headerNav?.map(({ title, url }) => (
+                <Link passHref href={url} key={title}>
+                  <StyledLink>{title}</StyledLink>
+                </Link>
+              ))}
+            </div>
           </div>
           <ThemeToggle />
         </div>
@@ -35,8 +38,8 @@ const SiteHeader = () => {
 export default SiteHeader;
 
 const StyledLink = styled('a', {
-  ...tw`text-gray-600`,
-  ...tw`inline-flex items-center px-4 height[40px] text-sm rounded cursor-pointer`,
+  ...tw`text-gray-600 rounded`,
+  ...tw`inline-flex items-center px-4 height[40px] text-xs tracking-wide cursor-pointer`,
   ...tw`hocus:(text-gray-900 bg-gray-300 bg-opacity-25 outline-none)`,
   '.dark &': {
     ...tw`text-gray-400`,
