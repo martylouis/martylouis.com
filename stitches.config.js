129af25c24371b10ff1c24f595336c7c577786be
Marty Thierry <marty@martylouis.com>
2021-10-06 15:05:26 -0500

    fix(stitches.config.js): update config to support latest version
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- stitches.config.js
+++ stitches.config.js
@@ -1,33 +1,25 @@
-// import { createCss } from '@stitches/react';
-// import { utils } from '@lib/stitches.utils';
+import { createStitches } from '@stitches/react';
 
-// export const {
-//   css,
-//   styled,
-//   global,
-//   theme,
-//   keyframes,
-//   getCssString,
-// } = createCss({
-//   media: {
-//     sm: '(min-width: 640px)',
-//     md: '(min-width: 768px)',
-//     lg: '(min-width: 1024px)',
-//   },
-//   utils,
-// });
-
-// stitches.config.ts
-import { createCss, StitchesCss } from '@stitches/react';
-// export type { StitchesVariants } from '@stitches/react'
-
-export const stitches = createCss({
-  prefix: '',
-  theme: {},
-  themeMap: {},
-  utils: {},
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    // colors: {
+    //   gray400: 'gainsboro',
+    //   gray500: 'lightgray',
+    // },
+  },
+  media: {
+    // bp1: '(min-width: 480px)',
+  },
+  utils: {
+    // marginX: (value) => ({ marginLeft: value, marginRight: value }),
+  },
 });
-
-// export type CSS = StitchesCss<typeof stitches>
-
-export const { css, styled, global, theme, keyframes, getCssString } = stitches;
