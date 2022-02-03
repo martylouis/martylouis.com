851b064c11294afa5f46d34fd6657f2a9d260533
Marty Thierry <marty@martylouis.com>
2022-02-03 15:07:21 -0600

    chore: move src files to src dir
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { getCssText } from '../stitches.config';
+import { getCssText } from '../../stitches.config';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
