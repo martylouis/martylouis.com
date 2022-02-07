50873913df178c1caa27e4c711920d7e397518d7
Marty Thierry <marty@martylouis.com>
2022-02-07 01:19:52 -0600

    wip: update blog posts to use contentlayer
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,33 +1,34 @@
-import { allPages } from '.contentlayer/data';
-import { Page as PageType } from '.contentlayer/types';
+import { allPosts } from '.contentlayer/data';
+import { Post as PostType } from '.contentlayer/types';
 import Container from '@/components/Container';
 import MdxComponents from '@/components/MdxComponents';
 import PageLayout from '@/components/PageLayout';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 
-const Pages = ({ page }: { page: PageType }) => {
-  const MDXContent = useMDXComponent(page.body.code);
+const BlogPosts = ({ post }: { post: PostType }) => {
+  const MDXContent = useMDXComponent(post.body.code);
   return (
     <PageLayout>
       <Container>
-        <h1>{page.title}</h1>
+        <h1>{post.title}</h1>
+        {console.log(post.body.code)}
         <MDXContent components={MdxComponents} />
       </Container>
     </PageLayout>
   );
 };
 
-export default Pages;
+export default BlogPosts;
 
 export const getStaticPaths: GetStaticPaths = () => {
   return {
-    paths: allPages.map((p) => ({ params: { slug: p.slug } })),
+    paths: allPosts.map((p) => ({ params: { slug: p.slug } })),
     fallback: false,
   };
 };
 
 export const getStaticProps: GetStaticProps = ({ params }) => {
-  const page = allPages.find((page) => page.slug === params.slug);
-  return { props: { page } };
+  const post = allPosts.find((post) => post.slug === params.slug);
+  return { props: { post } };
 };
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/Container';
+import PageLayout from '@/components/PageLayout';
+
+export default function BlogIndex() {
+  return (
+    <PageLayout>
+      <Container>
+        <h1>Blog</h1>
+      </Container>
+    </PageLayout>
+  );
+}
