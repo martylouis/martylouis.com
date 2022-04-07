b83cefd69d5f0c4e43b36ceaf64db10df2602943
Marty Thierry <marty@martylouis.com>
2022-04-06 20:13:00 -0500

    fix: update header size
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- components/Page/PageHeader.tsx
+++ components/Page/PageHeader.tsx
@@ -5,11 +5,8 @@ import ThemeToggle from '@/components/ThemeToggle';
 
 function PageHeader() {
   return (
-    <header
-      role="banner"
-      // tw="bg-white border-b border-black/5 dark:(bg-black/10 border-white/5)"
-    >
-      <Container size="full" tw="flex items-center justify-between py-4">
+    <header role="banner">
+      <Container size="lg" tw="flex items-center justify-between py-4">
         <Logo tw="w-12 h-12 text-gray-800 dark:text-gray-300" />
         <ThemeToggle />
       </Container>
