762bdb1866574d918ae94475ec4778c232d4f6e1
Marty Thierry <marty@martylouis.com>
2022-05-12 12:22:35 -0500

    fix(cwv): fix loading performance
    close ML-56
    
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { homeContent, homeSEO } from '@/data/pages/home.constants';
 import { projects } from '@/data/projects';
-import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { PaperPlaneRight } from 'phosphor-react';
 import tw from 'twin.macro';
@@ -9,50 +8,6 @@ import Container from '@/components/Container';
 import Page from '@/components/Page';
 import Project from '@/components/Project';
 import { SEOPage } from '@/components/SEO';
-
-function FadeInUp({ children }) {
-  return (
-    <motion.div
-      initial="hidden"
-      animate="show"
-      variants={{
-        hidden: {
-          opacity: 0,
-        },
-        show: {
-          opacity: 1,
-          transition: {
-            staggerChildren: 0.2,
-          },
-        },
-      }}
-    >
-      {children}
-    </motion.div>
-  );
-}
-
-function FadeInUpChild({ children }) {
-  return (
-    <motion.div
-      tw="w-full"
-      variants={{
-        hidden: { opacity: 0, y: 20 },
-        show: {
-          opacity: 1,
-          y: 0,
-          transition: {
-            type: 'spring',
-            stiffness: 100,
-            damping: 20,
-          },
-        },
-      }}
-    >
-      {children}
-    </motion.div>
-  );
-}
 
 export const Profile = ({ name, title, image }) => {
   const { src, alt, width, height } = image;
@@ -83,38 +38,29 @@ export const Hero = ({ title, subtitle, button }) => {
   const { url, text } = button;
   return (
     <Container tw="flex flex-col gap-8 py-16 md:gap-12 lg:py-24">
-      <FadeInUpChild>
-        <Profile {...homeContent.profile} />
-      </FadeInUpChild>
-      <FadeInUpChild>
-        <p
-          tw="text-xl lg:text-2xl"
-          dangerouslySetInnerHTML={{ __html: title }}
-        />
-      </FadeInUpChild>
-      <FadeInUpChild>
-        <p
-          tw="text-xl lg:text-2xl"
-          dangerouslySetInnerHTML={{ __html: subtitle }}
-        />
-      </FadeInUpChild>
-      <FadeInUpChild>
-        <p tw="flex justify-center pt-4">
-          <Button href={url} target="_blank" size="lg">
-            <span
-              dangerouslySetInnerHTML={{
-                __html: text,
-              }}
-            />
-            <PaperPlaneRight
-              tw="ml-1"
-              weight="fill"
-              aria-hidden="false"
-              focusable="false"
-            />
-          </Button>
-        </p>
-      </FadeInUpChild>
+      <Profile {...homeContent.profile} />
+
+      <p tw="text-xl lg:text-2xl" dangerouslySetInnerHTML={{ __html: title }} />
+
+      <p
+        tw="text-xl lg:text-2xl"
+        dangerouslySetInnerHTML={{ __html: subtitle }}
+      />
+      <p tw="flex justify-center pt-4">
+        <Button href={url} target="_blank" size="lg">
+          <span
+            dangerouslySetInnerHTML={{
+              __html: text,
+            }}
+          />
+          <PaperPlaneRight
+            tw="ml-1"
+            weight="fill"
+            aria-hidden="false"
+            focusable="false"
+          />
+        </Button>
+      </p>
     </Container>
   );
 };
@@ -124,35 +70,29 @@ export default function Home() {
     <Page>
       <SEOPage {...homeSEO} />
 
-      <FadeInUp>
-        <Hero {...homeContent.hero} />
+      <Hero {...homeContent.hero} />
 
-        <div tw="border-t border-gray-900/5 dark:border-gray-100/5" />
+      <div tw="border-t border-gray-900/5 dark:border-gray-100/5" />
 
-        <section tw="flex flex-col gap-16 py-16 lg:py-24">
-          <FadeInUpChild>
-            <Container tw="flex flex-col gap-4 text-center">
-              <h2
-                id="work"
-                tw="text-3xl font-bold text-gray-900 lg:text-5xl dark:text-gray-100"
-              >
-                Featured Work
-              </h2>
-              <p tw="font-medium lg:text-xl">
-                From small to large, here are just a few of the projects
-                I&rsquo;ve worked on.
-              </p>
-            </Container>
-          </FadeInUpChild>
-          <FadeInUpChild>
-            <Container size="lg" tw="grid gap-8 md:grid-cols-2">
-              {projects.map(({ ...project }) => (
-                <Project key={project.id} {...project} />
-              ))}
-            </Container>
-          </FadeInUpChild>
-        </section>
-      </FadeInUp>
+      <section tw="flex flex-col gap-16 py-16 lg:py-24">
+        <Container tw="flex flex-col gap-4 text-center">
+          <h2
+            id="work"
+            tw="text-3xl font-bold text-gray-900 lg:text-5xl dark:text-gray-100"
+          >
+            Featured Work
+          </h2>
+          <p tw="font-medium lg:text-xl">
+            From small to large, here are just a few of the projects I&rsquo;ve
+            worked on.
+          </p>
+        </Container>
+        <Container size="lg" tw="grid gap-8 md:grid-cols-2">
+          {projects.map(({ ...project }) => (
+            <Project key={project.id} {...project} />
+          ))}
+        </Container>
+      </section>
     </Page>
   );
 }
