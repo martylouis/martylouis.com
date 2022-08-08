834db7be2b500864db4223823d4e4c1273195cb2
Marty Thierry <marty@martylouis.com>
2022-08-08 18:20:08 -0500

    fix: remove splitbee analytics
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,8 @@
 import '../styles/global.css';
-import splitbee from '@splitbee/web';
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps, router }: AppProps) {
-  useEffect((): void => {
-    splitbee.init({
-      apiUrl: '/sb-api',
-      scriptUrl: '/sb.js',
-    });
-  }, []);
   return (
     <ThemeProvider
       disableTransitionOnChange
