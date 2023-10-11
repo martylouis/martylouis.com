8520a731e090246041ee8e4c35846b502e494622
Marty Thierry <marty@martylouis.com>
2023-10-11 17:09:14 -0500

    feat: add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx'
+import m2dx from 'astro-m2dx'
+
+const m2dxOptions = {
+  exportComponents: true,
+  autoImports: true,
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [
+    tailwind(),
+    mdx({
+      remarkPlugins: [[m2dx, m2dxOptions]],
+      extendDefaultPlugins: true,
+    }),
+  ],
+})
---
+++ src/content/_components.ts
@@ -0,0 +1,5 @@
+import H1 from '@/components/mdx/H1.astro'
+
+export const components = {
+  h1: H1,
+}
