afca2805b83fbf10b133feda537ebdda26988840
Marty Thierry <marty@martylouis.com>
2023-12-07 15:42:25 -0600

    chore(deps): add packages
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 import mdx from '@astrojs/mdx'
 import m2dx from 'astro-m2dx'
---
+++ src/lib/utils/cn.ts
@@ -0,0 +1,12 @@
+import { cx } from 'class-variance-authority'
+import type { ClassValue } from 'class-variance-authority/types'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * Returns a string of concatenated class names based on the input values.
+ * @param inputs - An array of class names or class name objects.
+ * @returns A string of concatenated class names.
+ */
+export const cn = (...inputs: ClassValue[]): string => {
+  return twMerge(cx(inputs))
+}
