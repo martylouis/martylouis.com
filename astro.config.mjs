fd401e5234a08c2880651f4d18633bac2deb5f2f
Marty Thierry <marty@martylouis.com>
2023-12-30 20:24:31 -0600

    chore: update Astro configuration and add new SVG icons
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import m2dx from 'astro-m2dx'
-import react from '@astrojs/react'
-import config from './src/data/config'
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import m2dx from "astro-m2dx";
+import react from "@astrojs/react";
+import config from "./src/data/config";
+import icon from "astro-icon";
 const m2dxOptions = {
   exportComponents: true,
   autoImports: true,
-}
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: config.site.url,
   base: config.site.paths.base,
-  trailingSlash: config.site.trailingSlash ? 'always' : 'never',
+  trailingSlash: config.site.trailingSlash ? "always" : "never",
   integrations: [
     tailwind({
       applyBaseStyles: false,
@@ -23,5 +24,11 @@ export default defineConfig({
       extendDefaultPlugins: true,
     }),
     react(),
+    icon({
+      include: {
+        "radix-icons": ["*"],
+      },
+      iconDir: "./src/assets/svg",
+    }),
   ],
-})
+});
