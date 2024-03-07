2fc781c1bac1b50b245e70d635339b2ff2cbde7d
Marty Thierry <marty@martylouis.com>
2024-03-06 18:28:52 -0600

    feat: update to tailwind v4 (#29)
    * Update package.json dependencies
    
    * Update tailwind.config.js file name to tailwind.config.mjs
    
    * Update tailwindcss version to 4.0.0-alpha.3
    
    * Comment out tailwind import and add tailwindcss plugin to vite
    
    * Remove unused tailwindcss configurations
    
    * Refactor color variables in global.css
    
    * Lint class order in ProjectCard and BaseLayout components
    
    * Update mode toggle icons and CSS variables
    
    * Refactor layout structure
    
    * Delete about page and its associated code
    
    * Refactor BaseFooter component to use dynamic copyright year
    
    * Refactor ProjectCard component to include project image and link
    
    * Refactor header and section layout in index.astro
    
    * Update Evolv Claims URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import m2dx from "astro-m2dx";
 import { defineConfig } from "astro/config";
@@ -15,9 +16,9 @@ export default defineConfig({
   site: "https://martylouis.com/",
   trailingSlash: "always",
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    // tailwind({
+    //   applyBaseStyles: false,
+    // }),
     mdx({
       remarkPlugins: [[m2dx, m2dxOptions]],
       extendDefaultPlugins: true,
@@ -30,4 +31,7 @@ export default defineConfig({
       iconDir: "./src/assets/svg",
     }),
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -61,8 +61,8 @@ export const ModeToggle: React.FC = () => {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="items-center" size="sm">
           <span className="relative">
-            <SunIcon className="absolute size-5 rotate-0 scale-100 transition-transform duration-150 dark:-rotate-90 dark:scale-0" />
-            <MoonIcon className="size-5 rotate-90 scale-0 transition-transform duration-150 dark:rotate-0 dark:scale-100" />
+            <SunIcon className="absolute size-5 rotate-lightmode scale-lightmode transition-transform duration-500" />
+            <MoonIcon className="size-5 rotate-darkmode scale-darkmode transition-transform duration-500" />
           </span>
           <span className="sr-only">Toggle theme</span>
           <CaretDownIcon className="-mr-0.5 ml-1 size-3" />
