05c9d38d05e6b986921a1572f4dc317be39f360d
Marty Thierry <marty@martylouis.com>
2024-08-10 01:09:47 -0500

    refactor(ThemeToggle): improve theme toggle logic and UI
    -  Refactor theme state management to use a constant array of theme objects.
    -  Simplify theme detection and application logic.
    -  Update icon sizes and class names for better UI consistency.
    -  Rename `theme-switcher.ts` to `theme-prefs.ts` for better clarity.
    - Move theme-prefs script to layout head
    
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -9,47 +9,42 @@ import { cn } from "@/lib/utils";
 import { CheckIcon, MoonStarIcon, SunIcon } from "lucide-react";
 import * as React from "react";
 
-type ThemeState = "light" | "dark" | "system";
+const themes = [
+  {
+    mode: "light",
+    name: "Light",
+  },
+  {
+    mode: "dark",
+    name: "Dark",
+  },
+  {
+    mode: "system",
+    name: "System",
+  },
+] as const;
+
+type ThemeMode = (typeof themes)[number]["mode"];
 
 export const ThemeToggle: React.FC = () => {
-  const [theme, setThemeState] = React.useState<ThemeState>("light");
+  const [themeMode, setThemeState] = React.useState<ThemeMode>("light");
 
-  const toggleTheme = (selectedTheme: ThemeState) => {
+  const toggleTheme = (selectedTheme: ThemeMode) => {
     setThemeState(selectedTheme);
   };
 
-  const handleThemeChange = React.useCallback(() => {
-    const isDarkMode = window.matchMedia(
-      "(prefers-color-scheme: dark)",
-    ).matches;
+  React.useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains("dark");
     setThemeState(isDarkMode ? "dark" : "light");
   }, []);
 
   React.useEffect(() => {
-    handleThemeChange();
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", handleThemeChange);
-    return () => {
-      window
-        .matchMedia("(prefers-color-scheme: dark)")
-        .removeEventListener("change", handleThemeChange);
-    };
-  }, [handleThemeChange]);
-
-  React.useEffect(() => {
-    if (
-      theme === "dark" ||
-      (theme === "system" &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [theme]);
-
-  const themes: ThemeState[] = ["dark", "light", "system"];
+    const isDark =
+      themeMode === "dark" ||
+      (themeMode === "system" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+  }, [themeMode]);
 
   return (
     <DropdownMenu>
@@ -59,14 +54,14 @@ export const ThemeToggle: React.FC = () => {
           className="group size-10 rounded-full bg-white/90 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
           size="sm"
           aria-label={`Change theme to ${
-            theme === "dark" ? "light" : "dark"
+            themeMode === "dark" ? "light" : "dark"
           } mode`}
         >
           <span className="relative">
             <SunIcon
-              size="6"
+              size="24"
               strokeWidth={1.5}
-              className="size-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600"
+              className="fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600"
             />
             <MoonStarIcon
               size={24}
@@ -78,18 +73,16 @@ export const ThemeToggle: React.FC = () => {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        {themes.map((themeItem, index) => (
-          <DropdownMenuItem key={index} onClick={() => toggleTheme(themeItem)}>
+        {themes.map((theme, index) => (
+          <DropdownMenuItem key={index} onClick={() => toggleTheme(theme.mode)}>
             <CheckIcon
               size={18}
               className={cn(
-                theme === themeItem ? "opacity-100" : "opacity-0",
+                themeMode === theme.mode ? "opacity-100" : "opacity-0",
                 "mr-2 transition-opacity delay-75",
               )}
             />
-            <span>
-              {themeItem.charAt(0).toUpperCase() + themeItem.slice(1)}
-            </span>
+            <span>{theme.name}</span>
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
