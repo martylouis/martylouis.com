1eb9a64b40e65eebe113865b01072a59d06698d3
Marty Thierry <marty@martylouis.com>
2024-08-16 19:56:29 -0500

    feat(seo): enhance SEO component and refactor layout
    -  Added `smartypants` dependency for improved title formatting.
    -  Refactored `BaseHead.astro` to use new `SEO.astro` component.
    -  Introduced `site.config.ts` for centralized site configuration.
    -  Deleted `Layout.astro` and `Metadata.astro`, replaced with `BaseLayout.astro`.
    -  Updated `index.astro` to use `BaseLayout.astro`.
    -  Renamed image path for better organization.
    -  Removed redundant metadata handling and improved canonical URL formatting.
    
    BREAKING CHANGE: Refactored layout and metadata handling, requiring updates to page components.
    
    Signed-off-by: Marty Thierry <marty@martylouis.com>

---
+++ src/data/site.config.ts
@@ -0,0 +1,55 @@
+export type SocialLink = {
+  platform: string;
+  href: string;
+  me?: string;
+  text: string;
+  icon: string;
+  footerOnly?: boolean;
+};
+
+export type SiteConfig = {
+  name: string;
+  title: string;
+  description: string;
+  image: {
+    src: string;
+    alt: string;
+  };
+  socialLinks: SocialLink[];
+};
+
+const siteConfig: SiteConfig = {
+  name: "Marty Louis | UX Designer & Developer",
+  title: "UX Designer & Developer",
+  description:
+    "I'm a UX Designer & Developer who loves to create beautiful and user-friendly websites.",
+  image: {
+    src: "/images/og/martylouis.jpg",
+    alt: "Let's make your website better",
+  },
+  socialLinks: [
+    {
+      platform: "github",
+      href: "https://github.com/martylouis/martylouis.com",
+      me: "https://github.com/martylouis/",
+      text: "Go to Marty's GitHub",
+      icon: "social/github",
+    },
+    {
+      platform: "twitter",
+      href: "https://twitter.com/martylouis",
+      me: "https://twitter.com/martylouis",
+      text: "Follow Marty on Twitter",
+      icon: "social/twitter",
+    },
+    {
+      platform: "linkedin",
+      href: "https://www.linkedin.com/in/martylouis/",
+      me: "https://www.linkedin.com/in/martylouis/",
+      text: "Follow Marty on LinkedIn",
+      icon: "social/linkedin",
+    },
+  ],
+};
+
+export default siteConfig;
