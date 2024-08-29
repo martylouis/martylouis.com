8d72239bc5888524f96c289524edcb5dca1d3e0d
Marty Thierry <marty@martylouis.com>
2024-08-29 09:36:31 -0500

    chore(config): migrate Prettier configuration to ESM
    Migrate Prettier configuration from .prettierrc to prettier.config.mjs, adopting ESM format. Update trailing comma rule to 'es5' and add JSDoc type annotation for better TypeScript support.
    
    Signed-off-by: Marty Thierry <marty@martylouis.com>

---
+++ prettier.config.mjs
@@ -0,0 +1,25 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
+      options: {
+        useTabs: false,
+      },
+    },
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  semi: false,
+  trailingComma: 'es5',
+}
+
+export default config
